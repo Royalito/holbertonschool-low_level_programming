ce55c55655086fa95bf500b557037c1ad37d6db3
Royalito <alejandronavarromorales98@gmail.com>
2023-02-28 06:23:14 -0800

    Alfabeto

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -2,14 +2,16 @@
 int main(void) 
 {
   int counter;
-  
-  
+  char newline = '\n';
   counter = 'a';
 
     while(counter <= 122)
   {
     putchar(counter);
     counter++;
+  }
+  {
+    putchar(newline);
   }
 
   return 0;
