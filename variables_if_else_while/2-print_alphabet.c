ab989893020d70b068aa1198adc19b76900dd31b
Royalito <alejandronavarromorales98@gmail.com>
2023-03-01 05:33:25 -0800

    Alfabeto

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
-int main(void) 
+int main(void)
 {
-  int counter;
-  char newline = '\n';
-  counter = 'a';
+int counter;
+char newline = '\n';
+ 
+counter = 'a';
 
-    while(counter <= 122)
-  {
-    putchar(counter);
-    counter++;
-  }
-  {
-    putchar(newline);
-  }
-
-  return 0;
+while (counter <= 122)
+{
+putchar(counter);
+counter++;
+}
+{
+putchar(newline);
+}
+return 0;
 }
