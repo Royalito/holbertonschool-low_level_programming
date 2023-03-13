3c349661124ef0870334d9ac7edb747475610faf
Royalito <alejandronavarromorales98@gmail.com>
2023-03-13 16:16:26 -0700

    Numeros y letras

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    char pepe;
+    char pepito;
+    char newline = '\n';
+
+    for (pepe = '0'; pepe <= '9'; pepe++)
+        putchar(pepe);
+    for (pepito = 'a'; pepito <= 'f'; pepito++)
+        putchar(pepito);
+    putchar(newline);
+    return (0);
+}
