6bad7e5e254f0bdca5c95371ae4102edca90f7c2
Royalito <alejandronavarromorales98@gmail.com>
2023-03-13 16:18:48 -0700

    Numeros y letras

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -9,14 +9,14 @@
  */
 int main(void)
 {
-    char pepe;
-    char pepito;
-    char newline = '\n';
+char pepe;
+char pepito;
+char newline = '\n';
 
-    for (pepe = '0'; pepe <= '9'; pepe++)
-        putchar(pepe);
-    for (pepito = 'a'; pepito <= 'f'; pepito++)
-        putchar(pepito);
-    putchar(newline);
-    return (0);
+for (pepe = '0'; pepe <= '9'; pepe++)
+putchar(pepe);
+for (pepito = 'a'; pepito <= 'f'; pepito++)
+putchar(pepito);
+putchar(newline);
+return (0);
 }
