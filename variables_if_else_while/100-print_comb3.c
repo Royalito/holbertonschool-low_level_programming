2a333503670c5beb4dab660b5e2ec70e3207181e
Royalito <alejandronavarromorales98@gmail.com>
2023-03-14 11:45:56 -0700

    Numeros con cero

---
+++ variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    int counter = 0;
+
+    while (counter < 10)
+    {
+        putchar(counter + '0');
+        if (counter  < 9)
+        {
+            putchar(',');
+            putchar(' ');
+            putchar('0');
+        }
+        counter++;
+    }
+    putchar('\n');
+
+    return (0);
+}
