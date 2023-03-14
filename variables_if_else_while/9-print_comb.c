e4ebd1bc5b43885cc1e23943c376d3a622a82432
Royalito <alejandronavarromorales98@gmail.com>
2023-03-14 11:29:56 -0700

    Numeros con comas

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    int counter = 0;
+
+    while (counter < 10)
+    {
+        putchar(counter + '0');
+        if (counter  < 9)
+        {
+            putchar(',');
+            putchar(' ');
+        }
+        counter++;
+    }
+    putchar('\n');
+
+    return (0);
+}
