4014bd0ae0442f1512356aeca79bef4dbf8b605e
Royalito <alejandronavarromorales98@gmail.com>
2023-03-24 06:41:20 -0700

    palabras

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int counter;
+int palabra1;
+int palabra2;
+int palabra3;
+int palabra4;
+int palabra5;
+int palabra6;
+int palabra7;
+char newline = '\n';
+counter = '_';
+palabra1 = 'p';
+palabra2 = 'u';
+palabra3 = 't';
+palabra4 = 'c';
+palabra5 = 'h';
+palabra6 = 'a';
+palabra7 = 'r';
+{
+putchar(counter);
+putchar(palabra1);
+putchar(palabra2);
+putchar(palabra3);
+putchar(palabra4);
+putchar(palabra5);
+putchar(palabra6);
+putchar(palabra7);
+}
+{
+putchar(newline);
+}
+return (0);
+}
