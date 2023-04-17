d19756a8b732efffac5acec64457ea7a1add1f8d
Royalito <alejandronavarromorales98@gmail.com>
2023-04-17 14:20:13 -0700

    Uno si es minuscula/mayuscula y cero si no

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Description: 'Uno si es minusca/mayuscula y cero si no lo es'
+ *
+ *
+ * Return: Always 0 (Success)
+ */
+
+int _isalpha(int Bodoke)
+{
+if (Bodoke >= 'A' && Bodoke <= 'z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
