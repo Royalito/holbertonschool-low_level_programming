4b6a04b36d2a0dc5b2bcffb7878df2c0c3142cbf
Royalito <alejandronavarromorales98@gmail.com>
2023-04-17 08:50:20 -0700

    Alfabeto

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -7,88 +7,17 @@
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 int counter;
-int palabra1;
-int palabra2;
-int palabra3;
-int palabra4;
-int palabra5;
-int palabra6;
-int palabra7;
-int palabra8;
-int palabra9;
-int palabra10;
-int palabra11;
-int palabra12; 
-int palabra13;
-int palabra15;
-int palabra16;
-int palabra17;
-int palabra18;
-int palabra19;
-int palabra20;
-int palabra21;
-int palabra22;
-int palabra23;
-int palabra24;
-int palabra25;
-int palabra26;
 char newline = '\n';
 counter = 'a';
-palabra1 = 'b';
-palabra2 = 'c';
-palabra3 = 'd';
-palabra4 = 'e';
-palabra5 = 'f';
-palabra6 = 'g';
-palabra7 = 'h';
-palabra8 = 'i';
-palabra9 = 'j';
-palabra10 = 'k';
-palabra11 = 'l';
-palabra12 = 'm'; 
-palabra13 = 'n';
-palabra15 = 'o';
-palabra16 = 'p';
-palabra17 = 'q';
-palabra18 = 'r';
-palabra19 = 's';
-palabra20 = 't';
-palabra21 = 'u';
-palabra22 = 'v';
-palabra23 = 'w';
-palabra24 = 'x';
-palabra25 = 'y';
-palabra26 = 'z';
+
+while (counter <= 122)
 {
 putchar(counter);
-putchar(palabra1);
-putchar(palabra2);
-putchar(palabra3);
-putchar(palabra4);
-putchar(palabra5);
-putchar(palabra6);
-putchar(palabra7);
-putchar(palabra8);
-putchar(palabra9);
-putchar(palabra10);
-putchar(palabra11);
-putchar(palabra12); 
-putchar(palabra13);
-putchar(palabra15);
-putchar(palabra16);
-putchar(palabra17);
-putchar(palabra18);
-putchar(palabra19);
-putchar(palabra20);
-putchar(palabra21);
-putchar(palabra22);
-putchar(palabra23);
-putchar(palabra24);
-putchar(palabra25);
-putchar(palabra26);
+counter++;
 }
 {
 putchar(newline);
