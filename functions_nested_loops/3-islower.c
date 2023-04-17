c3875933c97493e42626f7060e3e60a82a793414
Royalito <alejandronavarromorales98@gmail.com>
2023-04-17 13:48:43 -0700

    Uno si es minuscula y cero si no

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Description: 'imprime el alfabeto'
+ *
+ *
+ * Return: Always 0 (Success)
+ */
+
+int _islower(int Bodoke)
+{
+if (Bodoke >= 'a' && Bodoke <= 'z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
