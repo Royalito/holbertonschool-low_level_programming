e4774e0b86c573bcd0ac08fb916f2d2815de5308
Royalito <alejandronavarromorales98@gmail.com>
2023-04-17 11:49:24 -0700

    Afabeto por diez

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Description: 'imprime el alfabeto diez veces'
+ *
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet(void)
+{
+int tulio;
+char juanin;
+for (tulio = 0; tulio < 10; tulio++)
+{
+for (juanin = 'a'; juanin <= 'z'; juanin++)
+{
+_putchar(juanin);
+}
+_putchar ('\n');
+}
+}
