03345b792981c3a8511a62daf3b576906c1137bb
Royalito <alejandronavarromorales98@gmail.com>
2023-04-18 11:39:13 -0700

    Calculo del numero entero

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Description: 'Uno si es mayor a cero, cero si es igual cero y menos uno si no lo es'
+ *
+ *
+ * Return: Always 0 (Success)
+ */
+
+int _abs(int n)
+{
+if (n > 0)
+{
+return (n);
+}
+else
+{
+return (n * -1);
+}
+}
