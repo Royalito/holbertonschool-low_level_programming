5a8ee2faa66c34dff6d30b7af2b5773aac80ff8e
Royalito <alejandronavarromorales98@gmail.com>
2023-04-19 16:11:22 -0700

    tabla de multiplicar del nueve

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -10,34 +10,40 @@
 
 void times_table(void)
 {
-int juanin, tulio, c;
+int juanin;
+int tulio;
+int n;
+int d1;
+int d2;
 
 for (juanin = 0; juanin <= 9; juanin++)
 {
 for (tulio = 0; tulio <= 9; tulio++)
 {
-c = juanin * tulio;
+n = juanin * tulio;
 
-if (tulio == 0)
+if (n > 9)
 {
-_putchar(c + '0');
-}
-else if (c <= 9)
-{
-_putchar(32);
-_putchar(c + '0');
+d1 = n / 10;
+d2 = n % 10;
 }
 else
 {
-_putchar((c / 10) + '0');
-_putchar((c % 10) + '0');
+d1 = -16;
+d2 = n;
 }
-if (b != 9)
+if (tulio != 0)
+_putchar(d1 + 48);
+_putchar(d2 + 48);
+if (tulio == 9)
+{
+_putchar(10);
+}
+else
 {
 _putchar(',');
 _putchar(32);
 }
 }
-_putchar('\n');
 }
 }
