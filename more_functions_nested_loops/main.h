53f29b8708b84c387fd44b84d0b02c8d1711cf08
Royalito <alejandronavarromorales98@gmail.com>
2023-04-25 19:57:34 -0700

    Abanzando

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,13 +1,1 @@
-#include "main.h"
-
-/**
- * _isupper - Checks for uppercase character
- * @c: character
- * Return: 0 or 1
- */
-int _isupper(int c)
-{
-        if (c >= 65 && c <= 90)
-                return (1);
-        else
-                return (0);
+int _isupper(int c);
