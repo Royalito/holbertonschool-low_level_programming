d3e81993b5e9ec81dbc63ca4288d939309e57016
Royalito <alejandronavarromorales98@gmail.com>
2023-04-26 12:49:44 -0700

    lo mimsmo que el anterior

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+void print_numbers(void)
+{
+int a;
+
+for (a = 48; a <= 57; a++)
+{
+_putchar(a);
+}
+_putchar('\n');
+}
+
