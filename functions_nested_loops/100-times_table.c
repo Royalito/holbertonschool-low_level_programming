61c040a51484afaccbaf6f5171a1e3c357c58b93
Royalito <114327387+Royalito@users.noreply.github.com>
2023-07-31 10:17:47 -0500

    Update 100-times_table.c

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -1,23 +1,24 @@
 #include <stdio.h>
 
 void print_times_table(int n) {
-    // Check if n is within the valid range (0 to 15)
+    int i, j;
+    
+    /* Check if n is within the valid range (0 to 15) */
     if (n < 0 || n > 15) {
-        printf("Invalid input. n must be between 0 and 15.\n");
-        return;
+        return; // If n is out of range, do nothing and return from the function.
     }
 
-    // Print the n times table starting with 0
-    for (int i = 0; i <= n; i++) {
-        for (int j = 0; j <= n; j++) {
-            printf("%4d", i * j); // Use 4 spaces for better formatting
-        }
-        printf("\n");
+    for (i = 0; i <= 10; i++) {
+        printf("%d x %d = %d\n", n, i, n * i);
     }
 }
 
 int main() {
-    int n = 10; // Change this value to print a different times table
+    int n;
+    printf("Enter a number (0-15) to print its times table: ");
+    scanf("%d", &n);
+
     print_times_table(n);
+
     return 0;
 }
