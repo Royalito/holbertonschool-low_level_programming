2668ec55725badc559ec6d5332925d94199641af
Royalito <114327387+Royalito@users.noreply.github.com>
2023-08-05 17:15:57 -0500

    Update 6-puts2.c

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,22 +1,22 @@
-
+#include <stdio.h>
 /**
  * puts2 - prints one char out of 2 of a string.
- * @str: input string.
+ * @lucho: input string.
  * Return: no return.
  */
-void puts2(char *str)
+void puts2(char *lucho)
 {
 	int count = 0;
 
 	while (count >= 0)
 	{
-		if (str[count] == '\0')
+		if (lucho[count] == '\0')
 		{
 			_putchar('\n');
 			break;
 		}
 		if (count % 2 == 0)
-			_putchar(str[count]);
+			_putchar(lucho[count]);
 		count++;
 	}
 }
