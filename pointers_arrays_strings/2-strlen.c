49e724bdbda30283471b8ef88ee834a542e53a6a
Royalito <114327387+Royalito@users.noreply.github.com>
2023-08-05 15:43:13 -0500

    Update 2-strlen.c

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
+/**
+ * _strlen - returns the length of a string
+ *
+ * @pepe: string to be measured
+ *
+ * Return: amount of chars in string
+ */
 
+int _strlen(char *pepe)
+{
+	int length = 0;
+
+	for (; *pepe; pepe++)
+	{
+		length++;
+	}
+	return (length);
+}
